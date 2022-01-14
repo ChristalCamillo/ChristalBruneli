94c08ed86953b8e93f5d0cc95ec18103c2b91969
christal <christal.b.camillo@gmail.com>
2022-01-14 16:56:50 -0300

    Capturing manipulable html elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const img = document.querySelector('img');
+const button = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#name');
+const especie = document.querySelector('#species');
+const status = document.querySelector('#status');
