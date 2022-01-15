5b3ed164b5cfe95f2b30488a70dccdeaeada5ae9
christal <christal.b.camillo@gmail.com>
2022-01-15 15:29:25 -0300

    Final Commit

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
-const img = document.querySelector('img');
-const button = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#name');
-const especie = document.querySelector('#species');
-const status = document.querySelector('#status');
+document.addEventListener ('DOMContentLoaded', function (){
+	
+
+	document.querySelector('#botao') .addEventListener('click', function () {
+		let temp = []
+		const sections = document.querySelectorAll('.card')
+		for (let section of sections) {
+			const imagem = section.querySelector('.img')
+			const nome = section.querySelector('.nome')
+			const especie = section.querySelector('.especie')
+			const status = section.querySelector('.status')
+
+			let numeroAleatorio = gerarValorAletorio();
+			while (temp.indexOf(numeroAleatorio) >= 0) {
+				numeroAleatorio = gerarValorAletorio();
+			}
+			temp.push(numeroAleatorio)
+			fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+				method:'GET',
+				headers: {
+					Accept: 'application/json',
+					"Content-type": 'application/json'
+				}
+			}).then((response) => response.json()).then((data) => {
+				imagem.src = data.image;
+				imagem.alt = data.name;
+				nome.innerHTML = data.name;
+				especie.innerHTML = data.species;
+				status.innerHTML = traduzirCondicao(data);
+			});
+		}
+	})
+	
+	traduzirCondicao = (data) => {
+		if(data.status == 'unknown'){
+			return 'Não sabemos';
+		}else if(data.status == 'Alive'){
+			return 'Sim';
+		}else {
+			return 'Não. Está morto';
+		}
+	}
+
+	gerarValorAletorio = () => {
+		return Math.floor(Math.random() * 671);
+	}
+
+})
+
